c43f81897dc184d259466a1ea809a3204a7833e8
Jose Junior <junioroliveirace@gmail.com>
2024-01-23 00:58:52 -0300

    Criar construtor para receber DTO com dados para cadastro do médico.

--- src/main/java/med/voll/endereco/Endereco.java
+++ src/main/java/med/voll/endereco/Endereco.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Endereco {
     
+    
     private String logradouro;
     private String bairro;
     private String cep;
@@ -18,6 +19,16 @@ public class Endereco {
     private String complemento;
     private String cidade;
     private String uf;
+
+    public Endereco(DadosEndereco endereco) {
+        this.logradouro = endereco.logradouro();
+        this.bairro = endereco.bairro();
+        this.cep = endereco.cep();
+        this.numero = endereco.numero();
+        this.complemento = endereco.complemento();
+        this.cidade = endereco.cidade();
+        this.uf = endereco.uf();
+    }
 
 
 }
