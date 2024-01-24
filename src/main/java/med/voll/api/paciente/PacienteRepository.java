3ebaec0c46ae36d5320ecb1921b341f77613fa9a
Jose Junior <junioroliveirace@gmail.com>
2024-01-24 07:37:06 -0300

    Salvar cadastro de paciente.

--- src/main/java/med/voll/api/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/paciente/PacienteRepository.java
@@ -2,6 +2,6 @@ package med.voll.api.paciente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PacienteRepository extends JpaRepository<Long, Paciente>{
+public interface PacienteRepository extends JpaRepository<Paciente, Long>{
     
 }
