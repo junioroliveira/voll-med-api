3736c7c928531bff52b3be7b8e90dab24fff5202
Jose Junior <junioroliveirace@gmail.com>
2024-01-26 20:57:45 -0300

    Criar listagem para pacientes, com paginação e ordenação.

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,10 @@
 package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 
@@ -22,6 +27,10 @@ public class PacienteController {
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
         System.out.println(dados);
         pacienteRepository.save(new Paciente(dados));
+    }
+
+    public Page<DadosListagemPaciente> listar(@PageableDefault(sort = {"nome"}, size = 10, direction = Direction.ASC) Pageable paginacao){
+        return pacienteRepository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 
 }
