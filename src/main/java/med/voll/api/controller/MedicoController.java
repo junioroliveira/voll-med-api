785f3b2003890347b53ba3e6f6762f43d3057cc1
Jose Junior <junioroliveirace@gmail.com>
2024-01-26 22:55:54 -0300

    Criar resource para atualizar dados dos médicos.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,14 +5,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosAtualizacaoMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -25,6 +28,7 @@ public class MedicoController {
     private MedicoRepository medicoRepository;
 
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         System.out.println(dados);
         medicoRepository.save(new Medico(dados));
@@ -34,5 +38,13 @@ public class MedicoController {
     public Page<DadosListagemMedico> listar(@PageableDefault(sort = "nome", direction = Direction.ASC, page = 0, size = 10) Pageable paginacao){
         return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
     }
-    
+
+    @PutMapping
+    @Transactional
+    public void alterar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico = medicoRepository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+        //medicoRepository.save(medico); //produz o mesmo resultado @Transactional
+        //medicoRepository.flush(); //produz o mesmo resultado @Transactional
+    }  
 }
