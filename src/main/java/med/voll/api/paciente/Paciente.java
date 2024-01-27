dcd51359e19d224199963afc78f356236c014e03
Jose Junior <junioroliveirace@gmail.com>
2024-01-27 11:19:22 -0300

    Exclur logicamente pacientes - incluir propriedade ativo e criação de método para desativar na entidade paciente.

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -27,16 +27,22 @@ public class Paciente {
     private String email;
     private String telefone;
     private String cpf;
+    private Boolean ativo;
 
     @Embedded
     private Endereco endereco;
 
     public Paciente(DadosCadastroPaciente dados){
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void desativar(){
+        this.ativo = false;
     }
 
 }
