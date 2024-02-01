96ccb4fefbaf15d97befe2aa97807ae32455b234
Jose Junior <junioroliveirace@gmail.com>
2024-02-01 18:40:10 -0300

    Padronizar o retorno da API pacientes.

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosDetalhamentoPaciente;
 import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
@@ -28,21 +31,27 @@ public class PacienteController {
     PacienteRepository pacienteRepository;
     
     @PostMapping
-    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroPaciente dados, UriComponentsBuilder uriBuilder){
         System.out.println(dados);
-        pacienteRepository.save(new Paciente(dados));
+        var paciente = pacienteRepository.save(new Paciente(dados));
+
+        var uri = uriBuilder.path("/pacientes").buildAndExpand(paciente.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
     }
 
     @GetMapping
-    public Page<DadosListagemPaciente> listar(@PageableDefault(sort = {"nome"}, size = 10, direction = Direction.ASC) Pageable paginacao){
-        return pacienteRepository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+    public ResponseEntity<Page<DadosListagemPaciente>> listar(@PageableDefault(sort = {"nome"}, size = 10, direction = Direction.ASC) Pageable paginacao){
+         var page = pacienteRepository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+         return ResponseEntity.ok(page);
     }
 
     @PatchMapping("/{id}")
     @Transactional
-    public void desativar(@PathVariable Long id){
+    public ResponseEntity desativar(@PathVariable Long id){
         var paciente = pacienteRepository.getReferenceById(id);
         paciente.desativar();
+        return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
     }
 
 }
---
+++ src/main/java/med/voll/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,11 @@
+package med.voll.api.paciente;
+
+import med.voll.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(Long id, String nome, String email, String telefone, String cpf, Boolean ativo, Endereco endereco) {
+    
+    public DadosDetalhamentoPaciente(Paciente paciente){
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getAtivo(), paciente.getEndereco());
+    }
+
+}
